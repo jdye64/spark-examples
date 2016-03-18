2bdda4304fd9a5164d8ce4034e36ffbf2a3871c6
Jeremy Dyer <jdye64@gmail.com>
2016-03-18 15:00:33 -0400

    spark to nifi updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spark-examples
+
+spark-submit --master local --class com.jeremydyer.Main spark-nifi-1.0-SNAPSHOT.jar
--- src/main/java/com/jeremydyer/Main.java
+++ src/main/java/com/jeremydyer/Main.java
@@ -1,7 +1,10 @@
 package com.jeremydyer;
 
+import org.apache.nifi.remote.Transaction;
+import org.apache.nifi.remote.TransferDirection;
 import org.apache.nifi.remote.client.SiteToSiteClient;
 import org.apache.nifi.remote.client.SiteToSiteClientConfig;
+import org.apache.nifi.remote.exception.ProtocolException;
 import org.apache.nifi.spark.NiFiDataPacket;
 import org.apache.nifi.spark.NiFiReceiver;
 import org.apache.spark.SparkConf;
@@ -12,19 +15,41 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 /**
  * Created by jdyer on 3/15/16.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        SiteToSiteClientConfig config = new SiteToSiteClient.Builder()
-                .url("http://10.0.1.28:8090/nifi")
+        SiteToSiteClient.Builder builder = new SiteToSiteClient.Builder();
+        SiteToSiteClientConfig config = builder
+                .url("http://10.0.1.28:8080/nifi")
                 .portName("Data For Spark")
                 .buildConfig();
+
+        SiteToSiteClientConfig toNiFiConfig = builder
+                .url("http://10.0.1.28:8080/nifi")
+                .portName("Data From Spark")
+                .buildConfig();
+
+        SiteToSiteClient client = builder.fromConfig(toNiFiConfig).build();
+        final Transaction transaction = client.createTransaction(TransferDirection.SEND);
+        if (transaction == null) {
+            System.err.println("Unable to create a NiFi Transaction to send data");
+        }
+
+        //Bytes would be your flowfile content and map would be your desired flowfile attributes.
+        transaction.send("Hello Jeremy".getBytes(), new HashMap<String, String>());
+        transaction.confirm();
+        transaction.complete();
+
+        System.out.println("Data should be written to NiFi by now");
+
 
         SparkConf sparkConf = new SparkConf().setAppName("NiFi-Spark Streaming example");
         JavaStreamingContext ssc = new JavaStreamingContext(sparkConf, new Duration(10000L));
@@ -58,6 +83,8 @@ public class Main {
 //            }
 //        });
 
+        System.out.println("Spark Streaming successfully initialized");
         ssc.start();
+        ssc.awaitTermination();
     }
 }
